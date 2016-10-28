3221019f4a3b51659fa4c6e0f0c1f251512dbe62
System Administrator <root@Alexs-MBP.home>
2016-10-27 21:41:07 -0400

    initial scaffold commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name phoneBookApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the phoneBookApp
+ */
+angular.module('phoneBookApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
