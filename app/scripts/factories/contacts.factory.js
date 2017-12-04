f06b3fb73c584deb72a868af1d9a19741114959f
Alex Greco <agreco3@gatech.edu>
2017-12-04 10:46:51 -0500

    moved data into a seperate json file so that I can use http to get the data.

--- app/scripts/factories/contacts.factory.js
+++ app/scripts/factories/contacts.factory.js
@@ -1,63 +1,29 @@
 (function(){
 'use strict';
 
-	function contactFactory(){
-		//
+	function contactFactory($http){
+
 		var vm = this;
-		vm.contactDetail = {};
-		vm.contacts = {
-		    "1": {
-		      "firstName": "Steve",
-		      "lastName": "Anderson",
-		      "phoneNumber": "9082686574"
-		    },
-		    "2": {
-		      "firstName": "Andre",
-		      "lastName": "Roberts",
-		      "phoneNumber": "9188091885"
-		    },
-		    "3": {
-		      "firstName": "Shamika",
-		      "lastName": "Cranstin",
-		      "phoneNumber": "9732479330"
-		    },
-		    "4": {
-		      "firstName": "Pam",
-		      "lastName": "Halpert",
-		      "phoneNumber": "9732471113"
-		    },
-		    "5": {
-		      "firstName": "Jim",
-		      "lastName": "Halpert",
-		      "phoneNumber": "7326818847"
-		    }
-		};
-
-		
-		//
-
+		vm.contacts = $http.get("../../Data/contact-data.json").success(function(response){
+		  return response.data;
+    });
 
 
 		function getContacts(){
 			return vm.contacts;
 		}
 
-		function castContact(contact){
-			
-		}
-
 
 
 		//////////////////////////////////////////////////
 
-		var methods = {
-			getContacts : getContacts,
-			castContact: castContact
+		return {
+			getContacts : getContacts
 		};
 
-		return methods;
 	}
-	//contactFactory.$inject = ['$scope'];
+
+	contactFactory.$inject = ['$http'];
 
 	angular.module('phoneBookApp')
 	.factory('contactFactory', contactFactory);
